43658aa224aeb5115cfcac66fc3d57c46fc6da25
suhail.khan <su.khan@cgi.com>
2019-08-27 17:59:31 +0530

    interpolation concept app2

--- binding/src/app/test/test.component.ts
+++ binding/src/app/test/test.component.ts
@@ -3,12 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `  <h2> Welcome {{name}}</h2> 
-  <h2>{{2+9}} </h2>
-<h2> {{"Welcome "+ name}}</h2>  
-<h2>{{name.length}} </h2>
-<h2>{{name.toUpperCase()}} </h2>
-<h2>{{greetuser()}} </h2>
-<h2>{{siteurl}} </h2>
+ <input type="text" value="Suhail Khan">
   `,
   styles: []
 })
