6fc6206f778f308a29c377625117b2283ddef54d
suhail.khan <su.khan@cgi.com>
2019-08-27 15:12:25 +0530

    interpolation concept app

--- binding/src/app/test/test.component.ts
+++ binding/src/app/test/test.component.ts
@@ -2,13 +2,25 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  template: `  <h2> Welcome {{name}}</h2> 
+  <h2>{{2+9}} </h2>
+<h2> {{"Welcome "+ name}}</h2>  
+<h2>{{name.length}} </h2>
+<h2>{{name.toUpperCase()}} </h2>
+<h2>{{greetuser()}} </h2>
+<h2>{{siteurl}} </h2>
+  `,
+  styles: []
 })
 export class TestComponent implements OnInit {
 
+  public name= "suhail";
+  public siteurl= window.location.href;
   constructor() { }
-
+greetuser()
+{
+  return "Hello" + this.name;
+}
   ngOnInit() {
   }
 
