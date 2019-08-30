de5b20b793e76f813773a8f9e9152c5696b01a6a
suhail.khan <su.khan@cgi.com>
2019-08-30 09:26:31 +0530

    scenario 5 and 6 added

--- scenario5/src/app/component1/component1.component.ts
+++ scenario5/src/app/component1/component1.component.ts
@@ -2,16 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-component1',
-  template: `
-  <input [(ngModel)]="name" type="text"> 
-  {{name}} `,
-  styles: [ ]
+  templateUrl: './component1.component.html',
+  styleUrls: ['./component1.component.css']
 })
 export class Component1Component implements OnInit {
 
   constructor() { }
-
-  public name =" ";
 
   ngOnInit() {
   }
--- scenario6/src/app/app.module.ts
+++ scenario6/src/app/app.module.ts
@@ -3,13 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
-import {  }
- 
+import { Component1Component } from './component1/component1.component';
+import { Component2Component } from './component2/component2.component';
+import { Component3Component } from './component3/component3.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    Component1Component,
+    Component2Component,
+    Component3Component
   ],
   imports: [
     BrowserModule,
