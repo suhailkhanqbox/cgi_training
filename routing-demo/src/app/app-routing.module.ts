e179ffe9a69f3a01dadc5457fcb78d94ea8d16cc
suhail.khan <su.khan@cgi.com>
2019-09-01 14:34:27 +0530

    routing, wildcard routing with comments and theory

--- routing-demo/src/app/app-routing.module.ts
+++ routing-demo/src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DepartmentListComponent } from './department-list/department-list.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
+{path: '', redirectTo:'/departments',pathMatch:'full'}, //redirect when full url is empty
+//{path: '', component:DepartmentListComponent},//default route using component
 {path: 'departments', component:DepartmentListComponent},
-{path: 'employees', component:EmployeeListComponent}
+{path: 'employees', component:EmployeeListComponent},
+{path: "**", component: PageNotFoundComponent} //wild card route, 
+//it should be end of all path
 
 ];
 
@@ -15,4 +20,6 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents =[DepartmentListComponent, EmployeeListComponent]
+export const routingComponents =[DepartmentListComponent, 
+                                 EmployeeListComponent,
+                                 PageNotFoundComponent]
