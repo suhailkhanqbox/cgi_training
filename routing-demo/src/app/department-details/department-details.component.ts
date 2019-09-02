0216677183492d8bad15941ef8a8e8474df747f7
suhail.khan <su.khan@cgi.com>
2019-09-03 00:32:31 +0530

    previous and back button added, snapshot removed

--- routing-demo/src/app/department-details/department-details.component.ts
+++ routing-demo/src/app/department-details/department-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router,ParamMap } from '@angular/router';
+import { parse } from 'url';
 
 @Component({
   selector: 'app-department-details',
@@ -8,17 +9,35 @@ import { ActivatedRoute } from '@angular/router';
       department-details works!
       you selected deparment {{departmentId}}
     </p>
+    
+    <a (click)= "goPrevious()"> Previous</a>
+    <a (click)= "goNext()"> Next</a> 
   `,
   styles: []
 })
+//to navigate we need router service after adding click event
 export class DepartmentDetailsComponent implements OnInit {
 
   public departmentId;
-  constructor(private route: ActivatedRoute){} //import above then inject it 
+  constructor(private route: ActivatedRoute, private router: Router){} //import above then inject it 
 
   ngOnInit() {
-    let id = parseInt(this.route.snapshot.paramMap.get('id'));//paraMap to get paramneter from url
-    this.departmentId=id; 
+    //let id = parseInt(this.route.snapshot.paramMap.get('id'));//paraMap to get paramneter from url
+    //this.departmentId=id; 
+    this.route.paramMap.subscribe((params: ParamMap ) =>
+    {
+      let id =parseInt(params.get('id'));
+      this.departmentId=id;
+    });
   }
 
+  goPrevious(){
+    let previousId= this.departmentId-1;
+    this.router.navigate(['/departments',previousId]);
+  }
+
+  goNext(){
+    let nextId= this.departmentId+1;
+    this.router.navigate(['/departments',nextId]);
+  }
 }
