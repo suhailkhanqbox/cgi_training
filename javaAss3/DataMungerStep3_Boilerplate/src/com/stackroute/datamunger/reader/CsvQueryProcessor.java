cc37815ae2c90b0668e572f958c5162320b9362a
suhail.khan <su.khan@cgi.com>
2019-09-17 14:52:59 +0530

    ass3

--- javaAss3/DataMungerStep3_Boilerplate/src/com/stackroute/datamunger/reader/CsvQueryProcessor.java
+++ javaAss3/DataMungerStep3_Boilerplate/src/com/stackroute/datamunger/reader/CsvQueryProcessor.java
@@ -5,6 +5,8 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.opencsv.CSVReader;
 import com.stackroute.datamunger.query.DataTypeDefinitions;
@@ -61,13 +63,69 @@ public class CsvQueryProcessor extends QueryProcessingEngine {
 	@Override
 	public DataTypeDefinitions getColumnType() throws IOException {
 		DataTypeDefinitions d1= new DataTypeDefinitions();
+		BufferedReader br = new BufferedReader(new FileReader(this.s));
+		String text = br.readLine();
+		String line="";
+		String[] country={};
 
-		
+
+		while ((line = br.readLine()) != null)
+		{
+
+			// use comma as separator
+
+			country = line.split(",");
+			if (country.length == 18) {
+
+				break;
+			}
+		}
+		for(int i=0;i<country.length;i++)
+
+					{
+
+						String regex = "[+-]?[0-9][0-9]*";
+						// compiling regex
+						Pattern p1 = Pattern.compile(regex);
+						// Creates a matcher that will match input1 against regex
+						Matcher m1 = p1.matcher(country[i]);
+
+						String regex2="(\\d+\\.\\d+)";
+						Pattern p2= Pattern.compile(regex2);
+						Matcher m2= p2.matcher(country[i]);
+						// If match found and equal to input1
+
+
+									if(m1.find() && m1.group().equals(country[i]))
+									{   country[i]="java.lang.Integer";
+										System.out.println(country[i] + " is a valid integer number");
+									}
+
+
+									else if(m2.find() && m2.group().equals(country[i]))
+												{  country[i]= "java.lang.Double";
+
+												System.out.println(country[i] + " is a valid double number");
+												}
+
+									else
+									{
+										country[i]="java.lang.String";
+										System.out.println(country[i] + " is a valid String");
+									}
+
+
+					}
 
 
 
-
-
+		d1.setS1(country);
 		return d1;
+
+		}
+
+
+
+
 	}
-}
+
